7e5386604eba9d9e31e79347c7984962456f16d8
Fretty Debora Sirait <ifs22016@students.del.ac.id>
2025-07-12 11:58:02 +0700

    fix layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,9 +5,13 @@
     <meta charset="UTF-8">
     <title>Fretty S. | @yield('title', 'Portfolio')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- ✅ Tambahkan ini --}}
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+
     <link rel="stylesheet" href="{{ asset('css/about.css') }}">
     <link rel="stylesheet" href="{{ asset('css/service.css') }}">
-    
+
     <!-- Preload Lucide untuk memastikan dimuat lebih awal -->
     <link rel="preload" href="https://unpkg.com/lucide@0.441.0/dist/umd/lucide.min.js" as="script">
 </head>
@@ -15,6 +19,7 @@
 <body>
     @include('navbar')
     @yield('content')
+
     <!-- Pastikan Lucide dimuat sebelum script kustom -->
     <script src="https://unpkg.com/lucide@0.441.0/dist/umd/lucide.min.js"></script>
     <script src="{{ asset('js/service.js') }}"></script>
