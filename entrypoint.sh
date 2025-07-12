fa0cca81f62718a886f2216a675f15e2d6dbbf31
Fretty Debora Sirait <ifs22016@students.del.ac.id>
2025-07-12 09:57:13 +0700

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ if [ -n "$PORT" ]; then
   echo "==> Menggunakan PORT dari environment: $PORT"
   sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
   sed -i "s|<VirtualHost \*:80>|<VirtualHost *:${PORT}>|" /etc/apache2/sites-available/000-default.conf
+else
+  echo "==> WARNING: PORT is not set in environment!"
 fi
 
-# Eksekusi perintah utama
 exec "$@"
